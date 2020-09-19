70573324dc302afe9ce37c26c8146004972ac960
Mauro <molinamauro2@gmail.com>
2020-09-19 12:53:54 -0300

    add notes model and new note functionality

---
+++ src/models/Note.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const NoteSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    creationDate: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.model('Note', NoteSchema);
--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,7 +1,43 @@
 const router = require('express').Router();
+const Note = require('../models/Note');
 
-router.get('/notes', (req, res) => {
-    res.send('Notas acá');
+router.get('/notes/new', (req, res) => {
+    res.render('notes/newNote');
+})
+
+router.post('/notes/add', async (req, res) => {
+    const { title, description } = req.body;
+    const msg = [];
+    if(!title) {
+        msg.push({text: 'El título es obligatorio!'});
+    }
+    if(!description){
+        msg.push({text: 'La descripción es obligatoria!'})
+    }
+    if(msg.length > 0){
+        res.render('notes/newNote', {
+            msg,
+            title,
+            description
+        })
+    } else {
+        const note = new Note({
+            title,
+            description
+        });
+        await note.save();
+        msg.push({
+            text: 'Se agregó correctamente la nueva nota!'
+        })
+        res.redirect('/notes');
+    }
+})
+
+router.get('/notes', async (req, res) => {
+    const notes = await Note.find();
+    res.render('notes/notesList', {
+        notes
+    });
 })
 
 module.exports = router;
